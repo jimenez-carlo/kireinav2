50b448af96eaacdad8de0d5a047158bdf249ba5c
jimenez-carlo <jimenez.carlo.llabor@gmail.com>
2021-03-20 10:02:00 +0800

    kireina smartfilm update

--- script.js
+++ script.js
@@ -54,3 +54,20 @@ function closeForm() {
 }
 // Contact Us End
 
+// Index Tab
+function smartfilm_tab(evt, cityName) {
+  var i, tabcontent,tabcontent_2,tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent_2 = document.getElementsByClassName("tabcontent-2");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    tabcontent_2[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  docid(cityName).style.display = "block";
+  docid('sub_'+cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
